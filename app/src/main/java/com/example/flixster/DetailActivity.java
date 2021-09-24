dd9e005b42ae959a156090f27c63fe79b1794c06
amorita3 <amorita3@ivc.edu>
2021-09-23 19:48:31 -0700

    Commit

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -2,6 +2,7 @@ package com.example.flixster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.RatingBar;
@@ -24,7 +25,7 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyCjERsLcsEjw5Ki4SMKxGEP62fU0GhbO0I";
+    public static final String YOUTUBE_API_KEY = BuildConfig.YOUTUBE_API_KEY;
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
